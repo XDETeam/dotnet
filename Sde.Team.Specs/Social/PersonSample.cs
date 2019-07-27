1ffe0fb153fbf256fbd7612a0b6ff6b60fe4cb2d
stan <stanislav@yarmonov.pro>
2019-07-27 23:11:12 +0300

    =Person sample.

---
+++ Sde.Team.Specs/Social/PersonSample.cs
@@ -0,0 +1,27 @@
+﻿namespace Sde.Social
+{
+    /// <summary>
+    /// TODO:
+    /// </summary>
+    public class PersonSample
+    {
+        public static PersonSample Alice = new PersonSample
+        {
+            Id = 1,
+            FirstName = "Alice",
+            LastName = "Smith"
+        };
+
+        public static PersonSample Bob = new PersonSample
+        {
+            Id = 2,
+            FirstName = "Bob",
+            LastName = "Johnson"
+        };
+
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
