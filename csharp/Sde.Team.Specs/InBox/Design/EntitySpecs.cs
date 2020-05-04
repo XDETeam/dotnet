aed7b264a7b8072a718dc4321617093eeb4338be
stan <stanislav@yarmonov.pro>
2020-05-04 19:17:05 +0300

    +IEntity boilerplate.

---
+++ csharp/Sde.Team.Specs/InBox/Design/EntitySpecs.cs
@@ -0,0 +1,17 @@
+﻿namespace Sde.InBox.Design
+{
+    /// <summary>
+    /// TODO:Consider a a marker interface to classes those references
+    /// <typeparamref name="TEntity"/>. This way we can clearly understand 
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public interface IEntity<TEntity>
+    {
+
+    }
+
+    public class EntitySpecs
+    {
+
+    }
+}
