ec81b5bb8592af7753cca83d17099d7f68f81909
Stanislav S. Yarmonov <stanislav@yarmonov.pro>
2022-03-22 22:14:00 +0200

    *Solution build/run setup

--- src/Xde.Specs/Program.cs
+++ src/Xde.Specs/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+
+var version = typeof(Program)
+    .Assembly
+    ?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+    ?.InformationalVersion
+;
+
+Console.WriteLine($"XDE Spec. Version {version}");
