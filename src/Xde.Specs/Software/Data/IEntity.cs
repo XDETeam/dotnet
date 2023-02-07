c7138d157e36bfe92d34da1c124a60672b81783b
Stanislav S. Yarmonov <stanislav@yarmonov.pro>
2023-02-07 10:48:52 +0200

    +Data modeling

---
+++ src/Xde.Specs/Software/Data/IEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace Xde.Software.Data;
+
+/// <summary>
+/// Entity
+/// </summary>
+/// <typeparam name="TEntity">
+/// TODO:Entity class to which this one relates (the same entity, "partition" or
+/// other kind of relation).
+/// </typeparam>
+/// <remarks>
+/// TODO:Consider as a marker interface to classes those references
+/// <typeparamref name="TEntity"/>. This way we can clearly understand that some
+/// CreateUserRequest relates to the User entity and perform relevant code
+/// generation.
+/// </remarks>
+public interface IEntity<TEntity>
+{
+
+}
---
+++ src/Xde.Specs/Software/Data/NamespaceDoc.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Xde.Software.Data;
+
+/// <summary>
+/// Data models
+/// </summary>
+[CompilerGenerated]
+internal class NamespaceDoc
+{
+
+}
