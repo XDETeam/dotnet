5567b8957ec1f8e7b732f6b913f1978600897646
Stanislav S. Yarmonov <stanislav@yarmonov.pro>
2023-02-08 12:55:45 +0200

    +Development standards

---
+++ src/Xde.Specs/Software/Standards/NamespaceDoc.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Xde.Software.Standards;
+
+/// <remarks>
+/// TODO:Coding/developments standards implemented as a code
+/// </remarks>
+[CompilerGenerated]
+internal class NamespaceDoc
+{
+}
