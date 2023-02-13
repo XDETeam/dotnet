8cea4cd93c84f8eb9af220ba4ef112f67b3cc498
Stanislav S. Yarmonov <stanislav@yarmonov.pro>
2023-02-13 14:20:28 +0200

    +Configuration

---
+++ src/Xde.Specs/Software/Configuration/NamespaceDoc.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Xde.Software.Configuration;
+
+/// <summary>
+/// Configuration management
+/// </summary>
+/// 
+/// <remarks>
+/// TODO:
+/// Configuration as a code
+/// - Can be a SSoT generate all other configuration artifacts.
+/// - Has flexibility how to define configuration
+/// </remarks>
+[CompilerGenerated]
+internal class NamespaceDoc
+{
+
+}
