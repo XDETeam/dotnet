a69ff206b6f4feee1c3b4f28361ee9016d7c7280
Stanislav S. Yarmonov <stanislav@yarmonov.pro>
2023-02-28 20:01:48 +0200

    +Assembly description

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 XDE.Team toolkit for .NET solutions.
 
+TODO:Generate from Specs?
+
 ## Development environment
 - It's recommended to setup [Local nuget](ops/index.ipynb#local-nuget-repoisitory)
---
+++ src/Xde.Specs/AssemblyDoc.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// XDE.NET
+/// </summary>
+/// 
+/// <remarks>
+/// TODO:This is an experiment with idea close to xde.app (TODO:link) in the
+/// sense that it investigates benefits of focusing on integrity. "One ring
+/// to rule them all". Solution that can help to implement everything.
+/// 
+/// TODO:A good platform to test "Specs" concept, which is a way to implement
+/// "All-is-a-Code" idea. "Specs" is also kinda "One ring".
+/// 
+/// TODO:Implement README.md generation from the document like this.
+/// </remarks>
+public class AssemblyDoc
+{
+    //TODO:Include meta-information here? Or a separate meta-library?
+}
