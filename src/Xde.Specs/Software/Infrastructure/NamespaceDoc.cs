ad0765b9427ae250e0c75041ea709dceb68f00b1
Stanislav S. Yarmonov <stanislav@yarmonov.pro>
2023-03-02 22:05:44 +0200

    +Infrastructure notes

--- src/Xde.Specs/Software/Infrastructure/NamespaceDoc.cs
+++ src/Xde.Specs/Software/Infrastructure/NamespaceDoc.cs
@@ -7,6 +7,12 @@ namespace Xde.Software.Infrastructure;
 /// </summary>
 /// 
 /// <remarks>
+/// TODO:Consider example, when from the same architecture model define in the code
+/// we can create K8s/Podman manifests for different environments (Dev/Prod/Staging),
+/// generate documentation for <see cref="Xde.Software.Specs.SpecsServer"/>,
+/// and generate Docker/Podman environment inside integration test. So at least
+/// 3 rabbits with one model.
+/// 
 /// TODO:Temporary (?) included sample here <see cref="XdeArchitectureSample"/>
 /// </remarks>
 [CompilerGenerated]
